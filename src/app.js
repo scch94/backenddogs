7e2bc66c043608dec9bc813d6042a7008e092f1d
santiago canal <santycanal_06@hotmail.com>
2022-12-13 10:28:00 -0500

    back para deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const routes=require('./routes/index.js')
+const cors =require('cors')
 
 const server=express()
+
+server.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://proyecto-individual-6c9c.vercel.app");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+server.use(cors())
 server.use(express.json())
 server.use('/',routes)
 
